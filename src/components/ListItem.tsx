ea729e66ed630529093e1a3ea7457b17674c2274
Gavin Jin <gavinjin62@gmail.com>
2025-07-22 23:50:01 -0700

    update to 4.1 tailwind

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -7,9 +7,19 @@ interface ListItemProps {
 export default function ListItem({ item }: ListItemProps) {
   const slotInfo = item.slot_type ? `${item.slot_type}` : "-";
   return (
-    <li className="text-white customBorder">
-      <img src={item.iconUrl} alt={item.item} />
-      {item.item} | {item.rarity} | {slotInfo} | {item.type} | {item.price} gold
+    <li className={`customBorder flex items-center justify-between`}>
+      <div className="flex items-center justify-between min-w-1/3 legendaryGradient">
+        <img src={item.iconUrl} alt={item.item} className="max-w-5" />
+        <div style={{ color: `var(--color-${item.rarity.toLowerCase()})` }}>
+          {item.item} |
+        </div>
+        <div style={{ color: `var(--color-${item.rarity.toLowerCase()})` }}>
+          {item.rarity} |
+        </div>
+      </div>
+      <div>{slotInfo} |</div>
+      <div>{item.type} |</div>
+      <div>{item.price} gold</div>
     </li>
   );
 }
