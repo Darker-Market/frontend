9ea6d21c48d63400cbab218bff88099413a71296
Gavin Jin <gavinjin62@gmail.com>
2025-07-23 02:48:24 -0700

    change to grid styling

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -8,18 +8,23 @@ export default function ListItem({ item }: ListItemProps) {
   const slotInfo = item.slot_type ? `${item.slot_type}` : "-";
   const rarityInfo = item.rarity.toLocaleLowerCase();
   const textColor = `text-${rarityInfo}`;
+  const quantityInfo = item.quantity > 1 ? `(${item.quantity})` : "";
 
   return (
-    <li className="text-white customBorder flex items-center justify-between">
-      <div className="flex items-center justify-between min-w-1/3 legendaryGradient">
-        <img src={item.iconUrl} alt={item.item} className="max-w-5 max-h-5" />
-        {/* <div style={{ color: `var(--color-${item.rarity.toLowerCase()})` }}> */}
-        <div className={`${textColor}`}>{item.item} |</div>
-        <div className={`${textColor}`}>{item.rarity} |</div>
+    <li className="text-white text-center customBorder grid grid-cols-5 grid-rows-1 gap-4 items-center">
+      <div className="grid grid-cols-3 grid-rows-1 gap-4 items-center min-w-1/3 text-left">
+        <img
+          src={item.iconUrl}
+          alt={item.item}
+          className="max-w-15 max-h-15 min-w-15 min-h-15 object-contain"
+        />
+        <div className={`${textColor} w-100%`}>{item.item} </div>
+        <div className={`${textColor}`}>{item.rarity} </div>
       </div>
-      <div>{slotInfo} |</div>
-      <div>{item.type} |</div>
-      <div>{item.price} gold</div>
+      <div>{slotInfo} </div>
+      <div>{item.type} </div>
+      <div>expire time here</div>
+      <div>{`${item.price} ${quantityInfo}`} gold</div>
     </li>
   );
 }
