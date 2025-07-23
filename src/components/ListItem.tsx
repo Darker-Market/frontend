73538cdad5d1e6c3e4996a0c4a215d882459bd0e
Gavin Jin <gavinjin62@gmail.com>
2025-07-23 00:05:54 -0700

    switch to tailwind vite 4.1 to get it to work idk man why or how

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -7,7 +7,7 @@ interface ListItemProps {
 export default function ListItem({ item }: ListItemProps) {
   const slotInfo = item.slot_type ? `${item.slot_type}` : "-";
   return (
-    <li className={`customBorder flex items-center justify-between`}>
+    <li className="text-white customBorder flex items-center justify-between">
       <div className="flex items-center justify-between min-w-1/3 legendaryGradient">
         <img src={item.iconUrl} alt={item.item} className="max-w-5" />
         <div style={{ color: `var(--color-${item.rarity.toLowerCase()})` }}>
