e9be1db64f50d3bc5b9a381ec228ff7c4364b353
Gavin Jin <gavinjin62@gmail.com>
2025-07-25 19:20:13 -0700

    more space and center item image

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -11,12 +11,12 @@ export default function ListItem({ item }: ListItemProps) {
   const quantityInfo = item.quantity > 1 ? `(${item.quantity})` : "";
 
   return (
-    <li className="text-white text-center customBorder grid grid-cols-5 grid-rows-1 gap-4 items-center">
-      <div className="grid grid-cols-3 grid-rows-1 gap-4 items-center min-w-1/3 text-left">
+    <li className="text-white text-center customBorder grid grid-cols-6 grid-rows-1 gap-4 items-center">
+      <div className="grid grid-cols-3 grid-rows-1 gap-4 items-center min-w-1/3 text-left col-span-2 ">
         <img
           src={item.iconUrl}
           alt={item.item}
-          className="max-w-15 max-h-15 min-w-15 min-h-15 object-contain"
+          className="max-w-15 max-h-15 min-w-15 min-h-15 object-contain self-center justify-self-center"
         />
         <div className={`${textColor} w-100%`}>{item.item} </div>
         <div className={`${textColor}`}>{item.rarity} </div>
